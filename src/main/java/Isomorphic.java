71a1db0768c685da761ea3d6ff3a7bfbf0684d05
mudra <mudraverma@dal.ca>
2023-02-26 02:05:11 -0400

    Test case 2 -- Passed

--- src/main/java/Isomorphic.java
+++ src/main/java/Isomorphic.java
@@ -1,7 +1,12 @@
 public class Isomorphic {
     public boolean checkIsomorphic(String firstString, String secondString){
+
         if(firstString==null || secondString==null){
             return false;
+        }
+
+        if(firstString.isEmpty()==true && secondString.isEmpty()==true){
+            return true;
         }
         return false;
     }
