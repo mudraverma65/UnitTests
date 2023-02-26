d5d5e0abbbbc35b819b1221e42c26b409237f763
alensden <alensden@gmail.com>
2023-02-26 01:54:04 -0400

    Test case 2 -- incase of empty strings

--- src/test/java/IsomorphicTest.java
+++ src/test/java/IsomorphicTest.java
@@ -3,10 +3,26 @@ import static org.junit.Assert.*;
 
 public class IsomorphicTest {
     @Test
+    /**
+     * Checks if the strings passed is null
+     * If yes it should return false
+     * */
     public void testStringInputIfNull(){
         String stringInput1 = null;
         String stringInput2 = null;
         Isomorphic isomorphic = new Isomorphic();
         assertEquals(isomorphic.checkIsomorphic(stringInput1, stringInput2), false);
+    }
+
+    /**
+     * Checks if the strings passed is empty ""
+     * If yes it should return true
+     * */
+    @Test
+    public void testStringIsEmpty(){
+        String stringInput1 = "";
+        String stringInput2 = "";
+        Isomorphic isomorphic = new Isomorphic();
+        assertEquals(isomorphic.checkIsomorphic(stringInput1, stringInput2), true);
     }
 }
