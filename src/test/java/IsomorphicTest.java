a50d523cb2fb879efddd573756230aa76bd5207b
alensden <alensden@gmail.com>
2023-02-26 00:46:36 -0400

    Test case 1

---
+++ src/test/java/IsomorphicTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IsomorphicTest {
+    @Test
+    public void testStringInputIfNull(){
+        String stringInput1 = null;
+        String stringInput2 = null;
+        Isomorphic isomorphic = new Isomorphic();
+        assertEquals(isomorphic.checkIsomorphic(stringInput1, stringInput2));
+    }
+}
